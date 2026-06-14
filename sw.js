30f1d68c78f604e4c23de496a7a7258a5de289e9
yhk-13452 <772098245@qq.com>
2026-06-14 20:59:41 +0800

    Add service worker for caching shopping list assets

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'shopping-list-v1';
+const urlsToCache = [
+  '/shopping-list/',
+  '/shopping-list/index.html',
+  '/shopping-list/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
